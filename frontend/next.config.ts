a2a4e1bac7fe225e24051bee163c0258bc7c69ac
xAtomizerx <adam.baker9@snhu.edu>
2026-01-11 22:26:08 -0500

    adding next.config update for api calls through vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? '127.0.0.1*'
+          : '/backend/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;

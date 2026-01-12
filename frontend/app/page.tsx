7a2b826255224c5ccb99658969e042a8c2b44c84
xAtomizerx <adam.baker9@snhu.edu>
2026-01-11 22:56:59 -0500

    another push another update to urls

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const response = await fetch('https://recipe-two-jet.vercel.app/', { 
+  const response = await fetch('https://recipe-9811nf6hf-atomizers-projects.vercel.app/', { 
     cache: 'no-store' 
   });
 

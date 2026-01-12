e639db6e0272aebab814b95bdad06b501a7191e1
xAtomizerx <adam.baker9@snhu.edu>
2026-01-12 13:43:20 -0500

    Adding a new logo that will be used in the header component of the application. updated header with logo image.

--- frontend/app/header/page.tsx
+++ frontend/app/header/page.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import Image from 'next/image'
 import '../globals.css'
 import AuthRedirect from "../components/AuthRedirect"
 
@@ -6,7 +6,7 @@ export default function Header() {
     return(
         <div id='Header' className='text-4xl text-white p-4 flex justify-between items-center'>
             <div id='Title' className='font-bold background p-2 rounded'>
-            Hello World
+            <Image src='resources/RV_Logo.png' alt='Logo' width={150} height={100} className='inline-block mr-2'/>
             </div>
             <div id='AuthLinks' className='flex space-x-4 items-center'>
             <AuthRedirect type="signup"/>
